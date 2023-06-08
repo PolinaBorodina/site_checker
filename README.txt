13367d2fe2f71e7044af98b9c5d3756095a9282e
PolinaBorodina <135986588+PolinaBorodina@users.noreply.github.com>
2023-06-08 18:52:46 +0300

    v2
    Handled ctrl+c and invalid locators, replaced sample config file with another one

--- README.txt
+++ README.txt
@@ -3,20 +3,28 @@ pip install requests_html
 or just use requirements.txt
 pip install -r requirements.txt
 
+
 Config file usage:
 Write an url and xpath locator(s) with ; as a separator
+Possible formats:
+url;
+url;locator1
+url;locator1;locator2;
 Example:
 https://www.example.com/;//a[@href];//h1[contains(text(), 'Example Domain')]
 
 In this case this script checks site https://www.example.com/ and looks for elements with xpaths '//a[@href]' and '//h1[contains(text(), 'Example Domain')]'
 
-If no locators specified, the sript checks a status code only
+You can also use config.txt file in the project's directory as an example.
+
+If a site doesn't have specified locators, the sript checks a status code only
+
 
 Arguments to run:
 -h 	--help			help
 -ar 	--allow_redirect	to allow redirects. No redirects by default
 -d 	--delay			to set delay (in seconds). Default value - 60
 -t 	--timeout		to set request timeout (in seconds). Default value - 5
--cfg	--config_file		to change a path to the config file. Default path is "config.csv" in a project directory
+-cfg	--config_file		to change a path to the config file. Default path is "config.txt" in the project's directory
 -logs 	--logging_level		to change logging level: DEBUG, INFO, WARNING, ERROR. Default level - INFO
 -lfsize	--log_file_size		to change maximum size of a log file (in bytes). Default value - 20971520. The last log file will be backed up when the size limit is reached.
--- hw_tool.py
+++ hw_tool.py
@@ -1,9 +1,12 @@
 import argparse
 import logging
+import os
+import signal
 import sys
 from logging.handlers import RotatingFileHandler
 from time import time, sleep
 
+from lxml.etree import XPathEvalError
 from requests_html import HTMLSession
 
 arg_pars = argparse.ArgumentParser()
@@ -11,8 +14,8 @@ arg_pars.add_argument("-d", "--delay", default="60", type=int, help="change defa
 arg_pars.add_argument("-t", "--timeout", default="5", type=int, help="change default request timeout in seconds")
 arg_pars.add_argument("-ar", "--allow_redirect", default=False, action=argparse.BooleanOptionalAction,
                       help="add -ar if you want to allow redirects")
-arg_pars.add_argument("-cfg", "--config_file", default="config.csv",
-                      help="set a path to a config file, default path is 'config.csv' in a project directory")
+arg_pars.add_argument("-cfg", "--config_file", default="config.txt",
+                      help="set a path to a config file, default path is 'config.txt' in a project directory")
 arg_pars.add_argument("-logs", "--logging_level", default="INFO", type=str,
                       help="change a level of logs to write: DEBUG, INFO, WARNING, ERROR")
 arg_pars.add_argument("-lfsize", "--log_file_size", default="20971520", type=int, help="change max size of a log file")
@@ -22,7 +25,7 @@ logging.basicConfig(
     format='[%(asctime)s] %(levelname)s: %(message)s',
     level=args.logging_level,
     handlers=[RotatingFileHandler(filename='logs.txt', maxBytes=args.log_file_size, backupCount=1),
-              StreamHandler(stream=sys.stdout)])
+              logging.StreamHandler(stream=sys.stdout)])
 
 
 def get_configs():
@@ -72,14 +75,20 @@ def check_page(cfg):
             continue
 
         for element in elements:
-            elem = current_page.html.xpath(elements[elements.index(element)])
-            if not elem:
-                logging.warning(f"page: {url}, there is no expected element with xpath = {element}".rstrip('\n'))
-            else:
-                logging.debug(f"  page: {url}, element {elements.index(element)+1} is found")
+            try:
+                elem = current_page.html.xpath(elements[elements.index(element)])
+                if not elem:
+                    logging.warning(f"page: {url}, there is no expected element with xpath = {element}".rstrip('\n'))
+                else:
+                    logging.debug(f"  page: {url}, element {elements.index(element)+1} is found")
+            except XPathEvalError:
+                continue
 
 
 if __name__ == '__main__':
+    print("Press Ctrl+C to stop")
+    signal.signal(signal.SIGINT, lambda *_: os._exit(1))
+
     configs = get_configs()
     while True:
         check_page(configs)
